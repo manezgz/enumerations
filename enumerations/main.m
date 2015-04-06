1422c14ee0bc7edb58db770c6fe6515a12eead75
Jose Manuel Franco <manezgz@hotmail.com>
2015-04-06 21:06:17 +0200

    Initial Commit

---
+++ enumerations/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  enumerations
+//
+//  Created by Jose Manuel Franco on 6/4/15.
+//  Copyright (c) 2015 Jose Manuel Franco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
