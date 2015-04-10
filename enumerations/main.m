10cda6ae91a3f44844d22299703737e0a542b91d
Jose Manuel Franco <manezgz@hotmail.com>
2015-04-11 01:49:19 +0200

    Enumeraciones con bloques

--- enumerations/main.m
+++ enumerations/main.m
@@ -10,8 +10,37 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        //NSString
+        __block int ix=0;
+       // NSMutableString *test=@"Prueba";
+        
+        [@"Hola\nMundo" enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
+            
+            //test=@"hjkhkhj"; Peta ya que el puntero no puede cambiar
+            //[test appendString:@"Hi"];
+            //static int ix=0;
+            NSLog(@"Linea %d : %@",++ix,line);
+        }];
+        
+        
+       //NSArray
+        NSArray *episodes=@[@"IV",@"V",@"VI",@"I",@"II",@"III"];
+        
+        [episodes enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            NSLog(@"Episode %lu: %@",(unsigned long)idx,obj);
+        }];
+        
+        [episodes enumerateObjectsWithOptions:NSEnumerationConcurrent
+                                   usingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                                       NSLog(@"Episode %lu: %@",(unsigned long)idx,obj);
+                                   }];
+        
+        //NSDictioanry
+        NSDictionary *bso=@{@"Star Wars": @"John Williams",@"Inception":@"Hans Zimmer"};
+        [bso enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+            NSLog(@"La BSO de %@ ha sido compuesta por %@",key,obj);
+        }];
     }
     return 0;
 }
